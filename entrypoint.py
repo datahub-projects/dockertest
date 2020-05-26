2d7d2497542ccb7c6ed5afe95b4c371d63d4d55b
danx0r <dan.miller@eye0.com>
2020-05-26 14:38:42 -0700

    faster faster kill kill kill

--- entrypoint.py
+++ entrypoint.py
@@ -3,7 +3,7 @@ t0 = time.time()
 print ("This is a long-running process (of sorts")
 for i in range(10):
     print (i, "TIME:", time.time()-t0)
-    time.sleep(10)
+    time.sleep(1)
 print (i, "TIME:", time.time()-t0)
 print ("OK, done with that. I should be going to sleep now...")
 sys.stdout.flush()
--- go_to_sleep.py
+++
@@ -1,8 +0,0 @@
-import os, requests
-r=requests.get("http://169.254.169.254/latest/meta-data/instance-id")
-i=r.content.decode()
-cmd = "aws ec2 stop-instances --instance-ids %s" % i
-print (cmd)
-
-os.system(cmd)
-
