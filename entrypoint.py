a04556beaae7caafd37e732a2312d48a924e7960
danx0r <dan.miller@eye0.com>
2020-05-31 18:37:49 -0700

    mount vol

--- entrypoint.py
+++ entrypoint.py
@@ -1,9 +1,12 @@
 import sys, time, os
 t0 = time.time()
 print ("This is a long-running process (of sorts)")
+f = open("data/test.log", 'w')
 for i in range(10):
-    print (i, "TIME:", time.time()-t0)
+    t = time.time()-t0
+    print (i, "TIME:", t)
+    print (i, "TIME:", t, file=f)
     time.sleep(1)
-print (i, "TIME:", time.time()-t0)
+f.close()
 print ("OK, done with that. I should be going to sleep now...")
 sys.stdout.flush()
