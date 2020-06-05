8b5e8974b83c234ffc0cde492bdfdab959a449f3
danx0r <dan.miller@eye0.com>
2020-06-04 18:26:08 -0700

    absolutely

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,7 @@
 import sys, time, os
 t0 = time.time()
 print ("This is a long-running process (of sorts)")
-f = open("data/test.log", 'w')
+f = open("/data/test.log", 'w')
 for i in range(10):
     t = time.time()-t0
     print (i, "TIME:", t)
