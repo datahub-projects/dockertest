f059c110ffe1a0e9557876c2f151a40e224111b7
danx0r <dan.miller@eye0.com>
2020-07-08 10:28:39 -0700

    show IP addr

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt-get install -y python3
 RUN apt-get install -y python3-pip
 RUN apt-get install -y nano
 RUN apt-get install -y fish
+RUN apt-get install -y curl
 
 RUN adduser --disabled-password --gecos '' ubuntu
 
--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,8 @@
 import sys, time, os
 t0 = time.time()
+print ("Public ip:", end=' ')
+sys.stdout.flush()
+os.system("curl https://ipv4.wtfismyip.com/text")
 print ("This is a long-running process (of sorts)")
 print (os.path.abspath('.'))
 f = open("./data/test.log", 'w')
@@ -9,5 +12,6 @@ for i in range(10):
     print (i, "TIME:", t, file=f)
     time.sleep(1)
 f.close()
+
 print ("OK, done with that. I should be going to sleep now...")
 sys.stdout.flush()
